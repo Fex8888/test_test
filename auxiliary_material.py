e13eb90a22071e00030bb446a0c6703fdb7f3e34
Fex8888 <102014053+Fex8888@users.noreply.github.com>
2022-09-16 15:35:16 +0300

    Update auxiliary_material.py

--- auxiliary_material.py
+++ auxiliary_material.py
@@ -1,1 +1,14 @@
-# Список с названиями файловlist_files = [    'cat.jpeg',    'func.docx',    'server.png',    'include.rar',    'keywords.txt',    'text_1.txt',    'Yandex.html',    'CoolTermMac.zip',    'text_2.doc']
+
+
+# Список с названиями файлов
+list_files = [
+    'cat.jpeg',
+    'func.docx',
+    'server.png',
+    'include.rar',
+    'keywords.txt',
+    'text_1.txt',
+    'Yandex.html',
+    'CoolTermMac.zip',
+    'text_2.doc'
+]
