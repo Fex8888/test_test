4c4a57589a86827961228bbca3a0b41c8687008a
Fex8888 <102014053+Fex8888@users.noreply.github.com>
2022-09-16 15:33:29 +0300

    Add files via upload

---
+++ auxiliary_material.py
@@ -0,0 +1,1 @@
+# Список с названиями файловlist_files = [    'cat.jpeg',    'func.docx',    'server.png',    'include.rar',    'keywords.txt',    'text_1.txt',    'Yandex.html',    'CoolTermMac.zip',    'text_2.doc']
